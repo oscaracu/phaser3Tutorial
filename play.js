9f93915333ae646a51e9c1bf786c71f61bc1f815
Oscar del Angel Cu Reyes <oscaracu@miplazaweb.com.mx>
2022-08-05 09:12:48 +0000

    fixed KBinput dyanamics for playerHurt/playerStart

--- play.js
+++ play.js
@@ -3,32 +3,32 @@ class Play extends Phaser.Scene {
         super('playGame');
     }
 
-    playerStart(){
+    playerStart() {
 
-        
+
         if (this.player0.y > this.startPos) {
             this.player0.y -= 2;
             if (this.player0.y <= this.startPos) {
-                console.log(this.player0.y);
                 this.player0.setCollideWorldBounds(true);
                 this.startPos = config.height;
                 this.player0.body.checkCollision.none = false;
-                console.log(this.player0.body.checkCollision.none)
                 this.time.removeAllEvents();
+                this.playerActive = true;
+                this.player0.body.moves = true;
             }
         }
 
 
     }
 
-    
+
     shootBeam() {
         var beam = new Beam(this);
         beam.scale = 2.5;
     }
 
     enemyMoves(enemy) {
-        enemy.y += Phaser.Math.Between(1,3);
+        enemy.y += Phaser.Math.Between(1, 3);
         if (enemy.y > config.height) {
             this.enemyReset(enemy);
         }
@@ -41,23 +41,25 @@ class Play extends Phaser.Scene {
 
     }
 
-    playerHurt(){
+    playerHurt() {
+
+        this.playerActive = false;
 
         this.player0.play('explode');
-        
+
         this.player0.body.checkCollision.none = true;
-        
+
         this.time.addEvent({
             delay: 2500,
             callback: () => {
-                
+
                 this.player0.setAcceleration(0);
                 this.player0.setCollideWorldBounds(false);
                 this.startPos = config.height - 100;
                 this.player0.y = 700;
                 this.player0.x = config.width / 2;
                 this.player0.play('default');
-                
+
                 this.playerStart();
 
             }
@@ -70,9 +72,9 @@ class Play extends Phaser.Scene {
         this.load.spritesheet("background", "Space_BG.png", { frameWidth: 64, frameHeight: 64 });
         this.load.spritesheet("player", "Player/playerShip.png", { frameWidth: 16, frameHeight: 23 });
         this.load.image("beam", "Projectiles/Player_beam.png");
-        this.load.spritesheet("alan", "Enemies/Alan.png", { frameWidth: 16, frameHeight: 16});
-        this.load.spritesheet("bonbon", "Enemies/Bon_Bon.png", { frameWidth: 16, frameHeight: 16});
-        this.load.spritesheet("lips", "Enemies/Lips.png", { frameWidth: 16, frameHeight: 16});
+        this.load.spritesheet("alan", "Enemies/Alan.png", { frameWidth: 16, frameHeight: 16 });
+        this.load.spritesheet("bonbon", "Enemies/Bon_Bon.png", { frameWidth: 16, frameHeight: 16 });
+        this.load.spritesheet("lips", "Enemies/Lips.png", { frameWidth: 16, frameHeight: 16 });
         this.load.spritesheet("explosion", "Effects/Explosion.png", { frameWidth: 16, frameHeight: 16 });
     }
 
@@ -125,7 +127,7 @@ class Play extends Phaser.Scene {
 
         this.anims.create({
             key: 'alan1',
-            frames: this.anims.generateFrameNumbers('alan', {start: 3, end: 5}),
+            frames: this.anims.generateFrameNumbers('alan', { start: 3, end: 5 }),
             frameRate: 10,
             repeat: -1,
             yoyo: true
@@ -150,7 +152,8 @@ class Play extends Phaser.Scene {
         this.startPos = config.height - 100;
         this.alanStartPos = Phaser.Math.Between(20, config.width - 20);
         this.bonbonStartPos = Phaser.Math.Between(20, config.width - 20);
-        this.lipsStartPos = Phaser.Math.Between(20, config.width -20);
+        this.lipsStartPos = Phaser.Math.Between(20, config.width - 20);
+        this.playerActive = true;
 
         this.bgSprite = this.add.sprite(0, 0, "background").setVisible(false).play('bg');
         this.background = this.add.tileSprite(0, 0, config.width, config.height, "background");
@@ -193,26 +196,66 @@ class Play extends Phaser.Scene {
         }
 
         if (this.cursorKeys.left.isDown) {
-            
-            this.player0.anims.play('left', true).setAccelerationX(-2000).setVelocityX(-gameSettings.playerSpeed);
-            
+
+            if (this.playerActive === true) {
+
+                this.player0.anims.play('left', true).setAccelerationX(-2000).setVelocityX(-gameSettings.playerSpeed);
+
+            } else {
+
+                this.player0.setAcceleration(0);
+                this.player0.setVelocity(0);
+
+            }
+
+
         } else if (this.cursorKeys.right.isDown) {
-    
-            this.player0.anims.play('right', true).setAccelerationX(2000).setVelocityX(gameSettings.playerSpeed);
-    
-        } 
-    
-        if (this.cursorKeys.up.isDown) {
-    
-            this.player0.anims.play('forward', true).setAccelerationY(-5000).setVelocityY(-gameSettings.playerSpeed);
-            this.background.tilePositionY -= 2;
-    
-        } else if (this.cursorKeys.down.isDown) {
-    
-            this.player0.anims.play('default', true).setAcceleration(0, 2500).setVelocityY(gameSettings.playerSpeed/4);
-    
+
+            if (this.playerActive === true) {
+
+
+                this.player0.anims.play('right', true).setAccelerationX(2000).setVelocityX(gameSettings.playerSpeed);
+
+            } else {
+
+                this.player0.setAcceleration(0);
+                this.player0.setVelocity(0);
+
+            }
+
         }
-        
+
+        if (this.cursorKeys.up.isDown) {
+
+            if (this.playerActive === true) {
+
+
+                this.player0.anims.play('forward', true).setAccelerationY(-5000).setVelocityY(-gameSettings.playerSpeed);
+                this.background.tilePositionY -= 2;
+
+            } else {
+
+                this.player0.setAcceleration(0);
+                this.player0.setVelocity(0);
+
+            }
+
+        } else if (this.cursorKeys.down.isDown) {
+
+            if (this.playerActive === true) {
+
+
+                this.player0.anims.play('default', true).setAcceleration(0, 2500).setVelocityY(gameSettings.playerSpeed / 4);
+
+            } else {
+
+                this.player0.setAcceleration(0);
+                this.player0.setVelocity(0);
+
+            }
+
+        }
+
         for (var i = 0; i < this.projectiles.getChildren().length; i++) {
 
             var beam = this.projectiles.getChildren()[i];
@@ -224,7 +267,7 @@ class Play extends Phaser.Scene {
 
 
 
-    
+
 
 
 }
